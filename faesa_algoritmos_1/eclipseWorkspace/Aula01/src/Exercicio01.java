4010768ca675428a1d3c60d14e83921aed797377
Abrantes Araújo Silva Filho <abrantesasf@gmail.com>
2018-03-20 20:13:29 -0300

    Inclusão de exercício de aula, sobre operadores matemáticos

---
+++ faesa_algoritmos_1/eclipseWorkspace/Aula01/src/Exercicio01.java
@@ -0,0 +1,19 @@
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// Saída de operações
+		System.out.println("1.0 / 2 = " + (1.0 / 2));
+		System.out.println("\n1 / 2 = " + (1 /2));
+		System.out.println("\n1 % 2 = " + (1 % 2));
+		System.out.println("\n(200/10) % 4 = " + ((200/10) % 4));
+		System.out.println("\n5^2 + 3 = " + (Math.pow(5,2) + 3));
+		System.out.println("\n6 + 19 - 23 = " + (6 + 19 - 23));
+		System.out.println("\n3.0*5.0+1 = " + (3.0*5.0+1));
+		System.out.println("\n1/4+2 = " + (1/4+2));
+		System.out.println("\n29.0/7+4 = " + (29.0/7+4));
+		System.out.println("\n3/6.0-7 = " + (3/6.0-7));
+
+	}
+
+}
