aa9dd507bd1567d61459334bbafca5a605864681
Abrantes Araújo Silva Filho <abrantesasf@gmail.com>
2018-03-21 21:02:32 -0300

    Inclusão do exercício 1 da página 60 da apostila

---
+++ faesa_algoritmos_1/eclipseWorkspace/Aula01/src/Exercicio02_1.java
@@ -0,0 +1,73 @@
+// Importar Scanner
+import java.util.Scanner;
+
+// Classe principal
+public class Exercicio02_1 {
+
+	public static void main(String[] args) {
+		// Declara Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// Variáveis para receber os números
+		double n1 = 0;
+		double n2 = 0;
+		double n3 = 0;
+		
+		// Pega o primeiro número:
+		boolean ok = true;
+		while (ok) {
+			try {
+				System.out.print("\nEntre com a PRIMEIRA nota: ");
+				n1 = scan.nextDouble();
+				ok = false;
+			}
+			catch (Exception e) {
+				System.err.print("ERRO NA NOTA. \n");
+				scan.next();
+				ok = true;
+			}
+		}
+				
+		// Pega o segundo número:
+		ok = true;
+		while (ok) {
+			try {
+				System.out.print("\nEntre com a SEGUNDA nota: ");
+				n2 = scan.nextDouble();
+				ok = false;
+			}
+			catch (Exception e) {
+				System.err.print("ERRO NA NOTA. \n");
+				scan.next();
+				ok = true;
+			}
+		}
+		
+		// Pega o terceiro número:
+		ok = true;
+		while (ok) {
+			try {
+				System.out.print("\nEntre com a TERCEIRA nota: ");
+				n3 = scan.nextDouble();
+				ok = false;
+			}
+			catch (Exception e) {
+				System.err.print("ERRO NA NOTA. \n");
+				scan.next();
+				ok = true;
+			}
+		}
+		
+		// Fecha o scanner
+		scan.close();
+		
+		// Calcula média
+		double ponderada = (n1 * 2 + n2 * 3 + n3 * 5)/(2+3+5);
+		
+		// Imprime resultado:
+		System.out.printf("\nSuas notas foram %.2f, %.2f e %.2f. Sua média final foi de %.2f", n1, n2, n3, ponderada);
+		System.out.println("\n\nMoriturus te saluto.");
+
+	} // fecha public static void main
+
+} // fecha class
