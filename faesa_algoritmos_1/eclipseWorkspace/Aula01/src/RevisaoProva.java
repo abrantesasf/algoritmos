22576a153172ea8be9605930dc5277eb9873e4f2
abrantesasf <abrantesasf@gmail.com>
2018-04-21 15:17:36 -0300

    Revisão prova

---
+++ faesa_algoritmos_1/eclipseWorkspace/Aula01/src/RevisaoProva.java
@@ -0,0 +1,24 @@
+
+public class RevisaoProva {
+
+	public static void main(String[] args) {
+		String str = "100";
+		double a = Double.parseDouble(str);
+		System.out.println(a);
+		
+		System.out.println(10%4 >= 16%3);
+		
+		System.out.println(Math.pow(2, 3) < Math.sqrt(16) || 15/2 < 10 );
+		
+		int i;
+		long j = 10;
+		i = (int) j;
+		System.out.println(i);
+		
+		/*
+		 * Comentário em várias linhas
+		 */
+
+	}
+
+}
