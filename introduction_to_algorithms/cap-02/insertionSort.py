683b783c4699cc9e921ddeeafc7d9674803f972a
Abrantes Araújo Silva Filho <abrantesasf@gmail.com>
2019-08-19 21:23:56 -0300

    Atualização das implementações de exemplo do Insertion Sort

--- introduction_to_algorithms/cap-02/insertionSort.py
+++ introduction_to_algorithms/cap-02/insertionSort.py
@@ -5,27 +5,41 @@ Created on Tue Feb 27 14:21:48 2018
 @author: abrantesasf
 """
 
-def insertionSort(l):
+
+def insertion_sort(l):
     """
-    insertionSort:
-    função para realizar um sort com o algoritmo INSERTION SORT
-    
-    Pré-requisitos: nenhum
-            Inputs: uma lista de números
-           Outputs: imprime a lista ordenada
-        Limitações: não incluí checagens e validações
-         Sugestões: Abrantes Araújo Silva Filho
-                    abrantesasf@gmail.com
-       Referências: Cormen, Thomas H. et al. Introduction to Algorithms,
-                    3. ed., 2009.
-        Orientação: ---
-               URL: https://github.com/abrantesasf/algoritmos/tree/master/introduction_to_algorithms/cap-02
+    Faz a ordenação de uma lista de números em ordem ascendente,
+    utilizando o algoritmo de INSERTION SORT. A lista que é recebida
+    como parâmetro é modificada in-place.
+
+       Descrição: esta função implementa o algoritmo de INSERTION SORT
+                  (ordenação por inserção), um algoritmo eficiente para
+                  ordenar um número pequeno de elementos, com um
+                  parâmetro opcional para ordenar a lista em ordem
+                  crescente ou decrescente. O padrão é realizar a
+                  ordenação em ordem crescente.
+      Requisitos: -
+          Inputs: l     = lista de números
+         Outputs: imprime a lista ordenada
+         Returns: -
+     Observações: -
+    Complexidade: O(n^2)
+      Limitações: não inclui checagens e validações
+           To do: -
+     Referências: Cormen TH. et al. Introduction to Algorithms, 3. ed., 2009.
+      Pogramador: Abrantes Araújo Silva Filho
+       Copyright: -
+       Licensing: -
+       Sugestões: abrantesasf@gmail.com
+      Orientação: ---
+             URL: https://github.com/abrantesasf/algoritmos/tree/master/introduction_to_algorithms/cap-02
     """
     # Inicia Loop com o SEGUNDO número da lista:
     for j in range(1, len(l)):
-        # Pega o VALOR do segundo número da lista
+        # Pega o VALOR do segundo número da lista. O índice j aponta
+        # para o número atual que está sendo inserido na lista
         atual = l[j]
-        
+
         # Cria índice para o NÚMERO ANTERIOR na lista
         i = j - 1
 
@@ -38,16 +52,16 @@ def insertionSort(l):
 
             # Diminui o índice para checar o segundo anterior
             i = i - 1
-            
+
         # Se i < 0, então não há mais nenhum número anterior,
         # e podemos colocar o anterior com o valor atual
         l[i + 1] = atual
-        
-    # Retorna a lista ordenada
-    return print(l)
 
-a = [5, 2, 4, 6, 1, 3, 9, 11, 4, 0, 2, 4, 1, 9, 100]
-insertionSort(a)
+    # Imprime a lista ordenada
+    print(l)
 
+
+a = [5, 2, 4, 6, 1, 3, 9, 11, 4, 0, 1000, 2, 4, 1, 9, 100]
 b = [31, 41, 59, 26, 41, 58]
-insertionSort(b)
+insertion_sort(a)
+insertion_sort(b)
--- introduction_to_algorithms/cap-02/insertionSort2.py
+++ introduction_to_algorithms/cap-02/insertionSort2.py
@@ -5,36 +5,46 @@ Created on Wed Feb 28 11:07:04 2018
 @author: abrantesasf
 """
 
-def insertionSort2(l, ordem = 'asc'):
+
+def insertion_sort2(l, ordem='asc'):
     """
-    insertionSort2:
-    função para realizar um sort com o algoritmo INSERTION SORT, com a opção
-    de especificar se o sort será realizado em ordem ascendente (padrão) ou
-    em ordem descendente.
-    
-    Pré-requisitos: nenhum
-            Inputs: 1) l = uma lista de números
-                    2) ordem = uma string que pode ser 'asc' ou 'desc'
-                               e determina se o sort será realizado em
-                               ordem ascendente (padrão) ou descencente.
-           Outputs: imprime a lista ordenada
-        Limitações: não incluí checagens e validações
-         Sugestões: Abrantes Araújo Silva Filho
-                    abrantesasf@gmail.com
-       Referências: Cormen, Thomas H. et al. Introduction to Algorithms,
-                    3. ed., 2009.
-        Orientação: ---
-               URL: https://github.com/abrantesasf/algoritmos/tree/master/introduction_to_algorithms/cap-02
+    Faz a ordenação de uma lista de números em ordem ascendente ou
+    descendente utilizando o algoritmo de INSERTION SORT. A lista que é
+    recebida como parâmetro é modificada in-place.
+
+       Descrição: esta função implementa o algoritmo de INSERTION SORT
+                  (ordenação por inserção), um algoritmo eficiente para
+                  ordenar um número pequeno de elementos, com um
+                  parâmetro opcional para ordenar a lista em ordem
+                  crescente ou decrescente. O padrão é realizar a
+                  ordenação em ordem crescente.
+      Requisitos: -
+          Inputs: l     = lista de números
+                  ordem = <asc|desc>
+         Outputs: imprime a lista ordenada
+         Returns: -
+     Observações: -
+    Complexidade: O(n^2)
+      Limitações: não inclui checagens e validações
+           To do: -
+     Referências: Cormen TH. et al. Introduction to Algorithms, 3. ed., 2009.
+      Pogramador: Abrantes Araújo Silva Filho
+       Copyright: -
+       Licensing: -
+       Sugestões: abrantesasf@gmail.com
+      Orientação: ---
+             URL: https://github.com/abrantesasf/algoritmos/tree/master/introduction_to_algorithms/cap-02
     """
+
     # Inicia Loop com o SEGUNDO número da lista:
     for j in range(1, len(l)):
         # Pega o VALOR do segundo número da lista
         atual = l[j]
-        
+
         # Cria índice para o NÚMERO ANTERIOR na lista
         i = j - 1
-        
-        # Verifica ordem ascendente ou descentende
+
+        # Verifica ordem ascendente ou descendente
         if ordem == 'asc':
             # Inicia while verificando se o índice é >= 0 E se o
             # valor do número anterior na lista é maior do que o número atual
@@ -56,21 +66,22 @@ def insertionSort2(l, ordem = 'asc'):
                 # Diminui o índice para checar o segundo anterior
                 i = i - 1
         else:
-            return print('A ordem deve ser especificada como \'asc\' ou \'desc\'')
-            
+            return print('ERRO: a ordem deve ser especificada como \'asc\' ou \'desc\'')
+
         # Se i < 0, então não há mais nenhum número anterior,
         # e podemos colocar o anterior com o valor atual
         l[i + 1] = atual
-        
+
     # Retorna a lista ordenada
-    return print(l)
+    print(l)
+
 
 a = [5, 2, 4, 6, 1, 3, 9, 11, 4, 0, 2, 4, 1, 9, 100]
-insertionSort2(a, 'desc')
-insertionSort2(a, 'asc')
-
 b = [31, 41, 59, 26, 41, 58]
-insertionSort2(b)
-
 c = [3, 1, 4, 2]
-insertionSort2(c, 'desc')
+insertion_sort2(a, 'desc')
+insertion_sort2(a, 'asc')
+insertion_sort2(b)
+insertion_sort2(c, 'desc')
+insertion_sort2(c, 'errado')
+print(a)
